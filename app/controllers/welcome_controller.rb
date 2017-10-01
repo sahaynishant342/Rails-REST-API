c569a5c56721716dbde9e65ea67634abdf8e735c
Nishant Kumar <nishant.address@gmail.com>
2017-10-01 23:25:16 +0530

    added dashboard for showing system statistics

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    
+    @questions_count = Question.count
+    @answers_count = Answer.count
+    @users_count = User.count
+    @tenants = Tenant.all
   end
 end
