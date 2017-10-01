3e44e83272472f4b0fdd8f1acd16fe1caff37e80
Nishant Kumar <nishant.address@gmail.com>
2017-10-02 00:59:24 +0530

    added API count refresh functionality on dashboard for each tenant

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,4 +5,17 @@ class WelcomeController < ApplicationController
     @users_count = User.count
     @tenants = Tenant.all
   end
+
+  def fetch_api_count
+    if params[:id].nil?
+      render status: :ok, :json => { :api_count => -1 }.to_json
+      return
+    end
+    tenant = Tenant.find_by_id params[:id].to_i
+    if tenant.nil?
+      render status: :ok, :json => { :api_count => -1 }.to_json
+      return
+    end
+    render status: :ok, :json => { :api_count => tenant.api_count }.to_json
+  end
 end
